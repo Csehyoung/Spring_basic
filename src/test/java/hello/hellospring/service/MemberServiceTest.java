5dd528f03f6252b7516b0e416b95222adb86fdf8
Csehyoung <csehyoung@hanmail.net>
2021-11-22 22:49:01 +0900

    controller 추가

--- src/test/java/hello/hellospring/service/MemberServiceTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -3,20 +3,27 @@ package hello.hellospring.service;
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemoryMemberRepository;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class MemberServiceTest {
-    MemberService memberService = new MemberService();
-    MemoryMemberRepository memberRepository = new MemoryMemberRepository();
+    MemberService memberService;
+    MemoryMemberRepository memberRepository;
+
+    @BeforeEach
+    public void beforeEach() {
+       memberRepository = new MemoryMemberRepository();
+       memberService = new MemberService(memberRepository);
+
+    }
 
     @AfterEach
     public void afterEach() {
         memberRepository.clearStore();
     }
-
 
     @Test
     void join() {
